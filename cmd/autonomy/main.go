bf14842746bb0d07ae88d1bf35e4db80af31ebdf
saisunkari19 <saisunkari19@protonmail.com>
2021-06-19 12:16:40 +0530

    init blockchain config and adding Makefile

--- cmd/autonomy/main.go
+++ cmd/autonomy/main.go
@@ -4,7 +4,7 @@ import (
 	"os"
 
 	"github.com/AutonomyNetwork/autonomy-chain/app"
-	"github.com/AutonomyNetwork/autonomy-chain/cmd/autonomy-chaind/cmd"
+	"github.com/AutonomyNetwork/autonomy-chain/cmd/autonomy/cmd"
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 )
 
