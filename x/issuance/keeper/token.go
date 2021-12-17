7cbcf919278c95d94a2572cdb5c5ba4bedb935e4
saisunkari19 <saisunkari19@protonmail.com>
2021-12-17 11:14:14 +0530

    fix: query tokens by owner

--- x/issuance/keeper/token.go
+++ x/issuance/keeper/token.go
@@ -3,12 +3,12 @@ package keeper
 import (
 	"strconv"
 	"strings"
-	
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-	
+
 	"github.com/AutonomyNetwork/autonomy-chain/x/issuance/types"
-	
+
 	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
@@ -16,16 +16,16 @@ import (
 func (k Keeper) GetTokenCount(ctx sdk.Context) uint64 {
 	store := ctx.KVStore(k.storeKey)
 	bz := store.Get(types.TokenCountKey)
-	
+
 	if bz == nil {
 		return 0
 	}
-	
+
 	count, err := strconv.ParseUint(string(bz), 10, 64)
 	if err != nil {
 		panic("cannot decode count")
 	}
-	
+
 	return count
 }
 
@@ -44,24 +44,24 @@ func (k Keeper) IssueToken(
 ) uint64 {
 	// Create the token count
 	count := k.GetTokenCount(ctx)
-	
+
 	// Set the ID of the appended value
 	token.Id = count
-	
+
 	store := ctx.KVStore(k.storeKey)
 	bytes := k.cdc.MustMarshal(&token)
 	store.Set(types.GetTokenKey(token.Id), bytes)
-	
+
 	// Update token count
 	k.SetTokenCount(ctx, count+1)
-	
+
 	return count
 }
 
 // SetToken set a specific token in the store
 func (k Keeper) SetToken(ctx sdk.Context, token types.Token) {
 	store := ctx.KVStore(k.storeKey)
-	
+
 	b := types.MustMarshalToken(k.cdc, &token)
 	// b := k.cdc.MustMarshal(&token)
 	store.Set(types.GetTokenKey(token.Id), b)
@@ -94,22 +94,22 @@ func (k Keeper) RemoveToken(ctx sdk.Context, id uint64) {
 func (k Keeper) GetAllToken(ctx sdk.Context) (list []types.Token) {
 	store := ctx.KVStore(k.storeKey)
 	iterator := sdk.KVStorePrefixIterator(store, types.TokenKey)
-	
+
 	defer iterator.Close()
-	
+
 	for ; iterator.Valid(); iterator.Next() {
 		var val types.Token
 		k.cdc.Unmarshal(iterator.Value(), &val)
 		list = append(list, val)
 	}
-	
+
 	return
 }
 
 // Bank keeper functions
 
 func (k Keeper) MintToken(ctx sdk.Context, address string, amount sdk.Coin) error {
-	
+
 	sender, err := sdk.AccAddressFromBech32(address)
 	if err != nil {
 		return err
@@ -117,7 +117,7 @@ func (k Keeper) MintToken(ctx sdk.Context, address string, amount sdk.Coin) erro
 	if err := k.bankKeeper.MintCoins(ctx, types.ModuleName, sdk.Coins{amount}); err != nil {
 		return sdkerrors.Wrapf(err, "mint vouchers coins: %s", amount)
 	}
-	
+
 	if err := k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, sender, sdk.Coins{amount}); err != nil {
 		return sdkerrors.Wrap(err, "transfer tokens")
 	}
@@ -134,7 +134,7 @@ func (k Keeper) GetDenomMetaData(ctx sdk.Context, denom string) (bank.Metadata, 
 
 func (k Keeper) GetAllDenomsMetaData(ctx sdk.Context) []bank.Metadata {
 	denomsMetaData := make([]bank.Metadata, 0)
-	
+
 	k.bankKeeper.IterateAllDenomMetaData(ctx, func(metadata bank.Metadata) bool {
 		denomsMetaData = append(denomsMetaData, (metadata))
 		return false
@@ -158,27 +158,12 @@ func (k Keeper) GetDenomHolders(ctx sdk.Context, denom string) []bank.Balance {
 	return denomHolder
 }
 
-func (k Keeper)GetTokenByDenom(ctx sdk.Context, denom string) types.Token{
-	tokens:= k.GetAllToken(ctx)
-	for _, token := range  tokens{
-		if strings.EqualFold(denom, token.Denom){
+func (k Keeper) GetTokenByDenom(ctx sdk.Context, denom string) types.Token {
+	tokens := k.GetAllToken(ctx)
+	for _, token := range tokens {
+		if strings.EqualFold(denom, token.Denom) {
 			return token
 		}
 	}
 	return types.Token{}
 }
-
-func (k Keeper)GetTokensByOwner(ctx sdk.Context, addr string) []*types.Token {
-	var ownedTokens []*types.Token
-	
-	tokens:= k.GetAllToken(ctx)
-	for _, token := range tokens{
-		if strings.EqualFold(addr, token.Creator){
-			account := k.GetDenomHolders(ctx,token.Denom)
-			token.Holders  = uint64(len(account))
-			ownedTokens = append(ownedTokens, &token)
-		}
-	}
-	return ownedTokens
-}
-
